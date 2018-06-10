fd55deadb2ca9d4e9744cc566ec930be88a337e4
Avi Chad-Friedman <achadfried@sailthru.com>
2018-06-10 14:11:06 -0700

    WIP

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import sys
 import json
 from collections import Counter
 from flask import Flask, jsonify, render_template
+from pymongo import MongoClient
 
 FORMAT = '%(asctime)s  %(levelname)s    %(message)s'
 logging.basicConfig(level=logging.DEBUG, format=FORMAT, stream=sys.stdout)
@@ -21,6 +22,13 @@ webapp.config["DEBUG"] = True
 DEFAULT_PORT = 8080
 app = Flask(__name__)
 
+with open('moods.json') as f:
+    MOODS = json.load(f)
+
+def get_movies_collection():
+    client = MongoClient('localhost', 27017)
+    db = client.moodvies
+    return db['movies']
 
 @webapp.route("/moodvies", methods=['GET'])
 def load():
@@ -29,7 +37,14 @@ def load():
 
 @webapp.route("/mood/<mood>", methods=['GET'])
 def get_relevant_content(mood):
-    return jsonify({'mood': mood})
+    movie_collection = get_movies_collection()
+    filename, file_extension = os.path.splitext(mood)
+    print(MOODS)
+    cursor = movie_collection.find({"tags": {"$all": ['exciting']}})
+    for r in cursor:
+        print("yoyo")
+        print(r)
+    return jsonify({'mood': MOODS[filename]})
 
 if __name__ == '__main__':
     logging.info('loading data...')
--- scrape_netflix_to_mongo.py
+++ scrape_netflix_to_mongo.py
@@ -6,17 +6,23 @@ from pymongo import MongoClient
 def get_movies_collection():
     client = MongoClient('localhost', 27017)
     db = client.moodvies
-    return db['movies']
+    if db.movies.count() > 0:
+        db.movies.drop()
+    return db.movies
 
 def scrape():
     session = NetflixSession()
     for url in GENRE_SEARCH_URLS:
         session.load_cookies_into_browser()
         session.load_streaming_movies_from_page(search_url='https://www.netflix.com/browse/genre/34399')
-        print("URL BROO\n\n\n")
-        print(url)
-        print(session.movies)
     movie_collection = get_movies_collection()
+    cleaned_movies = session.movies
+    for movie in cleaned_movies:
+        genres = []
+        for genre in movie['genres']:
+            cleaned_genre = ''.join(c.lower() for c in genre if not c.isspace())
+            genres.append(cleaned_genre)
+        movie['genres'] = genres
     movie_collection.insert(session.movies)
 
 if __name__ == '__main__':
