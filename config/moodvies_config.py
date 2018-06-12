c29ca1af4f27ac828370fa0626002f485d549113
Avi Chad-Friedman <achadfried@sailthru.com>
2018-06-11 20:07:31 -0700

    bug fix in the scraper

--- config/moodvies_config.py
+++ config/moodvies_config.py
@@ -6,7 +6,11 @@ GENRE_SEARCH_URLS = {
     'movies' : {
         'dramas': 'https://www.netflix.com/browse/genre/5763?bc=34399',
         'action' : 'https://www.netflix.com/browse/genre/1365?bc=34399',
-        'comedies' : 'https://www.netflix.com/browse/genre/6548?bc=34399'
+        'comedies' : 'https://www.netflix.com/browse/genre/6548?bc=34399',
+        'classics': 'https://www.netflix.com/browse/genre/31574?bc=34399',
+        'cult': 'https://www.netflix.com/browse/genre/7627?bc=34399',
+        'documentaries': 'https://www.netflix.com/browse/genre/2243108?bc=34399',
+        'international': 'https://www.netflix.com/browse/genre/78367?bc=34399'
     }
 }
 
--- scrape_netflix_to_mongo.py
+++ scrape_netflix_to_mongo.py
@@ -1,6 +1,7 @@
 from lib.netflix_scraper import NetflixSession
 from config.moodvies_config import GENRE_SEARCH_URLS, MONGO
 from pymongo import MongoClient
+import pymongo
 
 
 MONGO_CONN = MONGO['conn_string']
@@ -14,19 +15,25 @@ def get_movies_collection():
 
 def scrape():
     session = NetflixSession()
+    movie_collection = get_movies_collection()
+    movie_collection.create_index([("title", pymongo.DESCENDING)],unique=True)
     for genre, url in GENRE_SEARCH_URLS.get('movies').items():
         print(url)
         session.load_cookies_into_browser()
         session.load_streaming_movies_from_page(search_url=url)
-    movie_collection = get_movies_collection()
-    cleaned_movies = session.movies
-    for movie in cleaned_movies:
-        genres = []
-        for genre in movie['genres']:
-            cleaned_genre = ''.join(c.lower() for c in genre if not c.isspace())
-            genres.append(cleaned_genre)
-        movie['genres'] = genres
-    movie_collection.insert(session.movies)
+        cleaned_movies = session.movies
+        for movie in cleaned_movies:
+            genres = []
+            for genre in movie['genres']:
+                cleaned_genre = ''.join(c.lower() for c in genre if not c.isspace())
+                genres.append(cleaned_genre)
+            movie['genres'] = genres
+            try:
+                movie_collection.insert(movie)
+            except pymongo.errors.DuplicateKeyError as e:
+                print(e)
+        #ordered=False so it attempts all document inserts
+        session.movies = []
 
 if __name__ == '__main__':
     scrape()
